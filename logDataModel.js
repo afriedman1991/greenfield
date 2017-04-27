a9eabda6216e0ec054ffe05b57ea04e8be72e50b
Anthony Greenheck <aemgreenheck@gmail.com>
2017-04-27 10:14:04 -0400

    server: initial log controller & model added

---
+++ logDataModel.js
@@ -0,0 +1,11 @@
+var mongose = require('mongoose');
+
+var LogDataSchema = new mongoose.Schema({
+  user: String,
+  level: Number,
+  mood: String,
+  note: String,
+  time: Date
+});
+
+module.exports = mongoose.model('Log', LogDataSchema);
