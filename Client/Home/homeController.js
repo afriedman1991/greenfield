15d02cde771763b9aea17acd762f4505662f8f47
Eric Zayas <ezayas16@icloud.com>
2017-04-27 10:51:25 -0400

    first controller commit

---
+++ Client/Home/homeController.js
@@ -0,0 +1,25 @@
+var app = angular.module('myApp',[])
+//may seperate controller and factory to adhere to the style guide
+
+
+.factory("postToServerFactory",function($scope,$http){
+  $scope.postToServer = function(){
+    $http({
+      method:"POST",
+      url: "/db",
+      data : {
+        'user': $scope.userInput.user,
+        'time': $scope.userInput.time,
+        'level': $scope.userInput.level,
+        'mood': $scope.userInput.mood,
+        'note' : $scope.userInput.note
+      }
+
+  }
+
+});
+
+.controller("submitController",function($scope,$http,postToServerFactory){
+   $scope.userInput = {};
+   $scope.submitInput = postToServerFactory.postToServer();
+ })
