ddb4459c284ab14feb48509d442066365bbfdd49
Guangyu Xu <davidxu198556@gmail.com>
2017-04-28 16:26:34 -0400

    add user table to database and it's features

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,10 +1,13 @@
 var logDataModel = require('../logData/log-data.model.js');//check this path!!!!!
-var logDataController = require('../logData/log-data.controller.js')//check this path!!!!!
+var logDataController = require('../logData/log-data.controller.js');//check this path!!!!!
+var userDataController = require('../userData/user-data.controller.js');
 var helpers = require('./helpers.js'); 
 
 module.exports = function (app, express) {
-	app.post('/db', logDataController.newLog);
-	app.get('/', logDataController.allLogs);
+  app.post('/db', logDataController.newLog);
+  app.get('/', logDataController.allLogs);
+  app.post('/signup', userDataController.newUser);
+  app.post('/signin', userDataController.signin;
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
---
+++ server/userData/user-data.controller.js
@@ -0,0 +1,35 @@
+var Q = require('q');
+var User = require('/user-data.model.js');
+
+var createUser = Q.nbind(User.create, User);
+
+module.exports = {
+  signin: function (req, res, next) {
+    var username = req.body.user;
+    var password = req.body.password;
+
+    findUser({user: user, password: password})
+    .than(function (user) {
+      if(!user) {
+        next(new Error("User does not exist"));
+      } else {
+        res.send('success');
+      }
+    })
+    .fail(function (error) {
+      next(error);
+    })
+  },
+
+
+  newUser: function (req, res, next) {
+    console.log("Received User Data from client: ", req.body);
+    return createUser({
+      user: req.body.user,
+      password: req.body.password
+    })
+    .fail(function (error) {
+      next(error);
+    })
+  }
+}
