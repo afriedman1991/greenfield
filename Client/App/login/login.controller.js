4894e2fd78f96c5cc1c8a6f7f2f11d37d9e7f056
Guangyu Xu <davidxu198556@gmail.com>
2017-05-04 00:45:36 -0400

    can do login, signup and before that, lock the scream to login

--- Client/App/login/login.controller.js
+++ Client/App/login/login.controller.js
@@ -1,5 +1,28 @@
 angular.module('app.login', [])
 
-.controller('loginController', function($scope, $http) {
+.controller('loginController', ['$scope', '$location', 'AuthService', function($scope, $location, AuthService) {
   
-})
+  $scope.login = function () {
+
+  	//initial values
+  	$scope.error = false;
+  	$scope.disabled = true;
+
+  	//call login from service
+  	AuthService.login($scope.user.username, $scope.user.password)
+
+  	//handle success
+  	.then(function () {
+  		$location.path('/');
+  		$scope.disable = false;
+  		$scope.user = {};
+  	})
+  	//handle error
+  	.catch(function() {
+  		$scope.error = true;
+  		$scope.errorMessage = "I'm sorry, your username or password may not currect";
+  		$scope.disable = true;
+  		$scope.user = {};
+  	});
+  };
+}]);
