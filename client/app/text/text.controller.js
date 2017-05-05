76d8bd0ce2dcc63be834f69c53ea343e166a2daf
Eric Zayas <ezayas16@icloud.com>
2017-05-05 16:57:24 -0400

    texting works trying to display messages sent from phone to app

--- client/app/text/text.controller.js
+++ client/app/text/text.controller.js
@@ -2,7 +2,7 @@ angular.module('app.text',[])
 .controller("textController",function($scope,$http){
   $scope.message = "";
   $scope.phoneNumber = "";
-  $scope.displayMessage = "";
+  $scope.displayMessage = ['hello','how are you?'];
   $scope.sendText = () => {
     $http({
       method: "POST",
@@ -15,5 +15,6 @@ angular.module('app.text',[])
     .then(function(response){
       displayMessage = response;
     })
+    $scope.displayMessage.push($scope.message);
   }
 })
