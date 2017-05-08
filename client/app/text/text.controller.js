d5a6102e592567d9a5dced06080638c8ddb86548
Anthony Greenheck <aemgreenheck@gmail.com>
2017-05-08 11:17:02 -0400

    fonts added and code clean up

--- client/app/text/text.controller.js
+++ client/app/text/text.controller.js
@@ -1,20 +1,34 @@
 angular.module('app.text',[])
 .controller("textController",function($scope,$http){
-  $scope.message = "";
-  $scope.phoneNumber = "";
-  $scope.displayMessage = ['hello','how are you?'];
-  $scope.sendText = () => {
-    $http({
-      method: "POST",
-      url: "/text",
-      data: {
-        message : $scope.message,
-        phoneNumber : $scope.phoneNumber
-      }
-    })
-    .then(function(response){
-      displayMessage = response;
-    })
-    $scope.displayMessage.push($scope.message);
-  }
+ $scope.message = "";
+ $scope.phoneNumber = "";
+ $scope.displayMessage = [];
+ $scope.sendText = () => {
+   $scope.displayMessage.push('Me:' + $scope.message);
+   let buttonClicked = false;
+
+   $http({
+     method: "POST",
+     url: "/message",
+     data: {
+       message : $scope.message,
+       phoneNumber : $scope.phoneNumber
+     }
+   })
+   .then(function(response){
+     if(buttonClicked === false){
+       $scope.displayMessage.push("MoodyBot: " + response.data);
+     }
+       buttonClicked = true;
+   })
+
+ }
+
+//will eventually be split into seperate controllers/factories
+ $scope.saveMessage = () => {
+   $http({
+     method: "GET",
+     url: "/text"
+   }).then((response) => response.status())
+ }
 })
