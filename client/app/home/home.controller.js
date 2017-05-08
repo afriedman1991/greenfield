45834244ac289450ea9894c3724534f214053069
Alex Friedman <Alex Friedman>
2017-05-08 10:37:53 -0400

    New Background!

--- client/app/home/home.controller.js
+++ client/app/home/home.controller.js
@@ -16,7 +16,8 @@ angular.module('app.home',['ngMaterial'])
 //   }
 // })
 
-.controller('homeController', ['$scope', '$http', '$mdDialog', 'AuthService', '$mdToast', function($scope, $http, $mdDialog, AuthService, $mdToast) {
+
+.controller('homeController', function($scope, $http, $mdDialog) {
   //Menu controls
   var originatorEv;
     this.openMenu = function($mdMenu, ev) {
@@ -25,39 +26,31 @@ angular.module('app.home',['ngMaterial'])
     };
 
   console.log('tessssssst')
-  $scope.options = {
-   level : null,
-   availableLevels: [3, 2, 1, 0, -1, -2, -3],
-   mood : null,
-   availableMoods: ['Content', 'So-so', 'Stressed', 'Upset', 'Motivated']
- };
 
-  // $scope.options = {
-  //    level : null,
-  //    availableLevels: [3, 2, 1, 0, -1, -2, -3],
-  //    mood : null,
-  //    availableMoods: ['sentiment_very_dissatisfied', 'sentiment_dissatisfied', 'sentiment_neutral', 'sentiment_satisfied', 'sentiment_very_satisfied'],
-  //    word: null,
-  //    words: ['Content', 'So-so', 'Stressed', 'Upset', 'Motivated']
-  //  };
+  $scope.options = {
+     level : null,
+     availableLevels: [3, 2, 1, 0, -1, -2, -3],
+     mood : null,
+     availableMoods: ['sentiment_very_dissatisfied', 'sentiment_dissatisfied', 'sentiment_neutral', 'sentiment_satisfied', 'sentiment_very_satisfied'],
+     word: null,
+     words: ['Content', 'So-so', 'Stressed', 'Upset', 'Motivated']
+   };
 
   let currDate = new Date();
-  $scope.time = new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate(), currDate.getHours());
-
+  $scope.time = new Date(currDate.getHours());
 
   $scope.submitInput = function() {
-    let username = AuthService.getUser();
-    console.log("My username is: ", username);
-    //let username = $scope.username;
-    let level = parseInt($scope.options.level, 10);
-    let mood = $scope.options.mood;
+    let username = $scope.username;
+    let level = availableMoods;
     let time = $scope.time;
-
+for (let i = 0; i < level.length; i++) {
+  if (mood === availableMoods[i]) {
+    level = availableLevels[i]
+  }
+}
     console.log('username', username);
     console.log('level', level);
     console.log(typeof level);
-    console.log('mood', mood);
-    console.log(typeof mood)
     console.log('time is', time);
 
     $http({
@@ -76,31 +69,4 @@ angular.module('app.home',['ngMaterial'])
   }
   originatorEv = null;
   // $scope.submitInput = postToServerFactory.postToServer();
-
-  // Toasts
-var last = {
-    bottom: false,
-    top: true,
-    left: false,
-    right: true
-  };
-
-  $scope.toastPosition = angular.extend({},last);
-
-  $scope.getToastPosition = function() {
-    return Object.keys($scope.toastPosition)
-      .filter(function(pos) { return $scope.toastPosition[pos]; })
-      .join(' ');
-  };
-
-  $scope.showSimpleToast = function() {
-    var pinTo = $scope.getToastPosition();
-
-    $mdToast.show(
-      $mdToast.simple()
-        .textContent('Thanks for logging!')
-        .position(pinTo )
-        .hideDelay(3000)
-    );
-  };
- }])
+});
